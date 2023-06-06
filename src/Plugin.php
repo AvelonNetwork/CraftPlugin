ef425b4e6eb3ecb12daa2a122af3d2bf66f06cc6
Martin Drahony <martin@honcho.agency>
2023-06-06 15:55:25 +0100

    craft 3 changes

--- src/Plugin.php
+++ src/Plugin.php
@@ -25,68 +25,81 @@ use yii\base\Event;
  */
 class Plugin extends BasePlugin
 {
-    public string $schemaVersion = '1.0.0';
-    public bool $hasCpSettings = false;
-    public bool $hasCpSection = true;
+	// public string $schemaVersion = '1.0.0';
+	// public bool $hasCpSettings = false;
+	// public bool $hasCpSection = true;
 
-    public static function config(): array
-    {
-        return [
-            'components' => ['settingsService' => SettingsService::class, 'avelonService' => AvelonService::class],
-        ];
-    }
+	/**
+	 * @var string
+	 */
+	public $schemaVersion = '3.0.11';
 
-    public function init()
-    {
-        parent::init();
+	/**
+	 * @var bool
+	 */
+	public $hasCpSection = true;
 
-        // Defer most setup tasks until Craft is fully initialized
-        Craft::$app->onInit(function () {
-            $this->attachEventHandlers();
-        });
+	/**
+	 * @var bool
+	 */
+	public $hasCpSettings = false;
 
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['avelon'] = 'avelon/settings/get-settings';
-            }
-        );
-    }
 
-    private function attachEventHandlers(): void
-    {
-        Event::on(
-            View::class,
-            View::EVENT_BEFORE_RENDER_TEMPLATE,
-            function (TemplateEvent $event) {
-                if (!Craft::$app->getRequest()->getIsCpRequest()) {
-                    $settings = $this->avelonService->getSettings();
-                    if ($settings) {
-                        $accountId = $settings['accountId'];
 
-                        if ($accountId) {
-                            Craft::$app->view->registerJsFile('https://' . $accountId . '.avln.me/t.js', ['position' => Craft::$app->view::POS_HEAD, 'async' => true, 'defer' => true]);
-                        }
-                    }
-                }
-            }
-        );
+	public function init()
+	{
+		parent::init();
 
-        Event::on(
-            Order::class,
-            Order::EVENT_AFTER_COMPLETE_ORDER,
-            function (Event $event) {
-                // get and format order data
-                $data = $this->avelonService->formatOrder($event);
+		// Install our event listeners
+		$this->attachEventHandlers();
 
-                if ($data && $data !== []) {
-                    // post the data to the api
-                    $this->avelonService->postToApi($data);
-                } else {
-                    Craft::error('Avelon: Order data could not be formatted', 'Avelon Plugin Error');
-                }
-            }
-        );
-    }
+
+		$this->setComponents([
+			'avelonService' => AvelonService::class,
+		]);
+
+		Event::on(
+			UrlManager::class,
+			UrlManager::EVENT_REGISTER_CP_URL_RULES,
+			function (RegisterUrlRulesEvent $event) {
+				$event->rules['avelon'] = 'avelon/settings/get-settings';
+			}
+		);
+	}
+
+	private function attachEventHandlers(): void
+	{
+		Event::on(
+			View::class,
+			View::EVENT_BEFORE_RENDER_TEMPLATE,
+			function (TemplateEvent $event) {
+				if (!Craft::$app->getRequest()->getIsCpRequest()) {
+					$settings = $this->avelonService->getSettings();
+					if ($settings) {
+						$accountId = $settings['accountId'];
+
+						if ($accountId) {
+							Craft::$app->view->registerJsFile('https://' . $accountId . '.avln.me/t.js', ['position' => Craft::$app->view::POS_HEAD, 'async' => true, 'defer' => true]);
+						}
+					}
+				}
+			}
+		);
+
+		Event::on(
+			Order::class,
+			Order::EVENT_AFTER_COMPLETE_ORDER,
+			function (Event $event) {
+				// get and format order data
+				$data = $this->avelonService->formatOrder($event);
+
+				if ($data && $data !== []) {
+					// post the data to the api
+					$this->avelonService->postToApi($data);
+				} else {
+					Craft::error('Avelon: Order data could not be formatted', 'Avelon Plugin Error');
+				}
+			}
+		);
+	}
 }
